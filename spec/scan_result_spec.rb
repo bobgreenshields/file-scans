07ae982da03d002dde93a19d7a8fee18b71bd5fb
Bob Greenshields <bob@thegreenshields.com>
2020-04-05 11:47:37 +0100

    ScanResult tested

--- spec/scan_result_spec.rb
+++ spec/scan_result_spec.rb
@@ -43,5 +43,37 @@ describe ScanResult do
 			end
 		end
 	end
+
+	describe '#files' do
+		context 'with files added' do
+			it 'enumerates a sorted list of the added files' do
+				folder = Folder.new(name: 'source', cloudroot: Pathname.pwd + '../fixture',
+														target: Pathname.pwd + '../fixture/target' )
+				sr = ScanResult.new(folder)
+				sr.add_file('file2.txt')
+				sr.add_file('file1.txt')
+				sr.add_file('file3.txt')
+				expected = %w(file1.txt file2.txt file3.txt)
+				result = sr.files.inject([]) {|arr, file| arr << file}
+				expect(result).to eql expected
+			end
+		end
+	end
+
+	describe '#new_dirs' do
+		context 'with new_dirs added' do
+			it 'enumerates a sorted list of the added new_dirs' do
+				folder = Folder.new(name: 'source', cloudroot: Pathname.pwd + '../fixture',
+														target: Pathname.pwd + '../fixture/target' )
+				sr = ScanResult.new(folder)
+				sr.add_new_dir('dir2')
+				sr.add_new_dir('dir1')
+				sr.add_new_dir('dir3')
+				expected = %w(dir1 dir2 dir3)
+				result = sr.new_dirs.inject([]) {|arr, new_dir| arr << new_dir}
+				expect(result).to eql expected
+			end
+		end
+	end
 	
 end
