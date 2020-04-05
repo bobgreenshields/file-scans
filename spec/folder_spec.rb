1fefb5ba1cc797232e483660616c54dadc56c447
Bob Greenshields <bob@thegreenshields.com>
2020-04-05 13:29:58 +0100

    More testing Folder

--- spec/folder_spec.rb
+++ spec/folder_spec.rb
@@ -3,5 +3,19 @@ require_relative '../lib/folder'
 include FileScans
 
 describe Folder do
+	describe '#path' do
+		it 'returns a Pathname' do
+			fixture = Pathname.new(__FILE__).dirname.expand_path + '../fixture/'
+			folder = Folder.new(name: 'test', cloudroot: fixture,
+													target: fixture + 'target' )
+			expect(folder.path).to be_a Pathname
+		end
+		it 'returns the correct path' do
+			fixture = Pathname.new(__FILE__).dirname.expand_path + '../fixture/'
+			folder = Folder.new(name: 'test', cloudroot: fixture,
+													target: fixture + 'target' )
+			expect(folder.path.to_s).to eql '/home/bobg/dev/file-scans/fixture/test'
+		end
+	end
 	
 end
