b0e945c70d989afc6fcc4eb1ee9c0513194ffb43
Bob Greenshields <bob@thegreenshields.com>
2020-04-07 09:06:24 +0100

    Add exit methods to FileScan

--- lib/file_scan.rb
+++ lib/file_scan.rb
@@ -14,25 +14,51 @@ module FileScans
 		end
 
 		def config_hash
-			@config_hash ||= YAML.load(rcfile.read)
+			@config_hash ||= YAML.load(@rcfile.read)
 		end
 
 		def load_folders
-			exit_missing_config_key("cloudroot") unless config_hash.key?('cloudroot')
-			@cloudroot = Pathname.new(config_hash['cloudroot'])
+			exit_if_config_key_missing("cloudroot")
+			@cloudroot = Pathname.new(@config_hash['cloudroot'])
 			exit_cloudroot_not_exist unless cloudroot.directory?
-			exit_missing_config_key("folders") unless config_hash.key?('folders')
-			config_hash['folders'].each { | folder_hash | load_folder(folder_hash) }
+			exit_if_config_key_missing("folders")
+			@config_hash['folders'].each { | folder_hash | load_folder(folder_hash) }
 		end
 
 		def load_folder(folder_hash)
-			exit_missing_config_key("name") unless config_hash.key?('name')
-			exit_missing_config_key("target") unless config_hash.key?('target')
-			target = Pathname.new(name: config_hash["name"])
+			exit_if_config_key_missing("name")
+			exit_if_config_key_missing("target")
+			target = Pathname.new(name: @config_hash["name"])
 			exit_folder_target_not_exist(target) unless target.exist?
-			@folders << Folder.new(name: config_hash["name"], target: target, 
+			@folders << Folder.new(name: @config_hash["name"], target: target, 
 														 cloudroot: @cloudroot)
 			self
+		end
+
+		def exit_no_rc
+			STDERR.puts "Was expecting to find a config file called .filescan"
+			STDERR.puts "at #{@rcfile}"
+			STDERR.puts "but one could not be found"
+			exit(65)
+		end
+
+		def exit_if_config_key_missing(key)
+			return if @config_hash.key?(key)
+			STDERR.puts "Looking in the config file for a key called #{key}"
+			STDERR.puts "but none could be found"
+			exit(66)
+		end
+
+		def exit_cloudroot_not_exist
+			STDERR.puts "Looking for the cloudroot directory"
+			STDERR.puts "at #{@cloudroot} but it could not be found"
+			exit(67)
+		end
+
+		def exit_folder_target_not_exist(target)
+			STDERR.puts "Looking for the target directory"
+			STDERR.puts "at #{target} but it could not be found"
+			exit(68)
 		end
 	end
 
