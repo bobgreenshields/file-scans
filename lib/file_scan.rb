85d1f6978faa48aecc12e123f1d12bcb91e23d4f
Bob Greenshields <bob@thegreenshields.com>
2020-04-09 08:58:32 +0100

    Simplify FileScan

--- lib/file_scan.rb
+++ lib/file_scan.rb
@@ -6,35 +6,27 @@ module FileScans
 	class FileScan
 		attr_reader :cloudroot
 
-		def initialize
+		def initialize(config_hash)
+			@config_hash = config_hash
 			@folders = []
 		end
 
-		def rcfile
-			@rcfile ||= Pathname.new(ENV["HOME"]) + ".filescanrc"
-			exit_no_rc unless @rcfile.exist?
-		end
-
-		def config_hash
-			@config_hash ||= YAML.load(@rcfile.read)
+		def cloudroot
+			return @cloudroot if @cloudroot
+			@cloudroot = Pathname.new(@config_hash['cloudroot'])
+			exit_if_dir_not_exist(name: "cloud dir", dir: @cloudroot)
+			@cloudroot
 		end
 
 		def load_folders
-			exit_if_config_key_missing("cloudroot")
-			@cloudroot = Pathname.new(config_hash['cloudroot'])
-			exit_cloudroot_not_exist unless cloudroot.directory?
-			exit_if_config_key_missing("folders")
-			config_hash['folders'].each { | folder_hash | load_folder(folder_hash: folder_hash,
-																																cloudroot: @cloudroot) }
+			@config_hash['folders'].each { | folder_hash | load_folder(folder_hash)}
 		end
 
-		def load_folder(folder_hash: , cloudroot: )
-			exit_if_folder_key_missing(key: "name", hash: folder_hash)
-			exit_if_folder_key_missing(key: "target", hash: folder_hash)
+		def load_folder(folder_hash)
+			name = folder_hash["name"]
 			target = Pathname.new(folder_hash["target"])
-			exit_folder_target_not_exist(target) unless target.exist?
-			@folders << Folder.new(name: folder_hash["name"], target: target,
-														 cloudroot: cloudroot)
+			exit_if_dir_not_exist(name: "the target of folder #{name}", dir: target)
+			@folders << Folder.new(name: folder_hash["name"], target: target, cloudroot: cloudroot)
 			self
 		end
 
@@ -43,38 +35,50 @@ module FileScans
 			@folders.each { |folder| yield folder}
 		end
 
-		def exit_no_rc
-			STDERR.puts "Was expecting to find a config file called .filescan"
-			STDERR.puts "at #{@rcfile}"
-			STDERR.puts "but one could not be found"
-			exit(65)
+		def exit_if_dir_not_exist(name: ,dir:)
+			dir_path = Pathname.new(path)
+			return if dir_path.directory?
+			STDERR.puts "Looking for #{name} at #{path}"
+			if dir_path.exist?
+				STDERR.puts "It is not a directory"
+			else
+				STDERR.puts "It does not exist"
+			end
+			exit(70)
 		end
 
-		def exit_if_config_key_missing(key)
-			return if config_hash.key?(key)
-			STDERR.puts "Looking in the config file for a key called #{key}"
-			STDERR.puts "but none could be found"
-			exit(66)
-		end
+		# def exit_no_rc
+		# 	STDERR.puts "Was expecting to find a config file called .filescan"
+		# 	STDERR.puts "at #{@rcfile}"
+		# 	STDERR.puts "but one could not be found"
+		# 	exit(65)
+		# end
 
-		def exit_cloudroot_not_exist
-			STDERR.puts "Looking for the cloudroot directory"
-			STDERR.puts "at #{@cloudroot} but it could not be found"
-			exit(67)
-		end
+		# def exit_if_config_key_missing(key)
+		# 	return if @config_hash.key?(key)
+		# 	STDERR.puts "Looking in the config file for a key called #{key}"
+		# 	STDERR.puts "but none could be found"
+		# 	exit(66)
+		# end
 
-		def exit_folder_target_not_exist(target)
-			STDERR.puts "Looking for the target directory"
-			STDERR.puts "at #{target} but it could not be found"
-			exit(68)
-		end
+		# def exit_cloudroot_not_exist
+		# 	STDERR.puts "Looking for the cloudroot directory"
+		# 	STDERR.puts "at #{@cloudroot} but it could not be found"
+		# 	exit(67)
+		# end
 
-		def exit_if_folder_key_missing(key: ,hash:)
-			return if hash.key?(key)
-			STDERR.puts "Looking in the folder info for a key called #{key}"
-			STDERR.puts "but none could be found"
-			exit(69)
-		end
+		# def exit_folder_target_not_exist(target)
+		# 	STDERR.puts "Looking for the target directory"
+		# 	STDERR.puts "at #{target} but it could not be found"
+		# 	exit(68)
+		# end
+
+		# def exit_if_folder_key_missing(key: ,hash:)
+		# 	return if hash.key?(key)
+		# 	STDERR.puts "Looking in the folder info for a key called #{key}"
+		# 	STDERR.puts "but none could be found"
+		# 	exit(69)
+		# end
 	end
 
 
