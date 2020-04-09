f8600ca077bc0591befb134d420c6159ad0ee8c9
Bob Greenshields <bob@thegreenshields.com>
2020-04-09 22:13:32 +0100

    Improve formatting DefaultScanFormatter

--- lib/default_scan_formatter.rb
+++ lib/default_scan_formatter.rb
@@ -2,36 +2,39 @@
 module FileScans
 	class DefaultScanFormatter
 		def call(scan_result)
-			result = files(scan_result)
+			result = []
+			result += new_dirs(scan_result)
+			result += files(scan_result)
+			result.join("\n")
 		end
 
 		def files(scan_result)
 			case scan_result.files.count
 			when 0
-				["There are no files to move"]
+				["\nThere are no files to move"] << "\n"
 			when 1
-				result = ["There is one file"]
+				result = ["\nThere is one file"]
 				result += scan_result.files.map(&:to_s)
-				result << "To file"
+				result << "\n"
 			else
-				result = ["There are #{scan_result.files.count} files"]
+				result = ["\nThere are #{scan_result.files.count} files"]
 				result += scan_result.files.map(&:to_s)
-				result << "To file"
+				result << "\n"
 			end
 		end
 
 		def new_dirs(scan_result)
 			case scan_result.new_dirs.count
 			when 0
-				["There are no new dirs to create"]
+				["\nThere are no new dirs to create"]
 			when 1
-				result = ["There is one new dir"]
+				result = ["\nThere is one new dir"]
 				result += scan_result.new_dirs.map(&:to_s)
-				result << "To create"
+				# result << "To create"
 			else
-				result = ["There are #{scan_result.new_dirs.count} new dirs"]
+				result = ["\nThere are #{scan_result.new_dirs.count} new dirs"]
 				result += scan_result.new_dirs.map(&:to_s)
-				result << "To create"
+				# result << "To create"
 			end
 		end
 
