5d1577232ead2f6c7609dcda0814922457b719f7
Bob Greenshields <bob@thegreenshields.com>
2020-04-12 14:43:40 +0100

    FSParser rename dirs option to build_dirs

--- lib/fs_parser.rb
+++ lib/fs_parser.rb
@@ -9,10 +9,10 @@ module FileScans
 
 		def make_opt_parser
 			@opt_parser = OptionParser.new do | opts |
-				opts.on '-d', '--dirs', 'Make the empty directory structure' do @options << :dirs end
+				opts.on '-d', '--dirs', 'Make the empty directory structure' do @options << :build_dirs end
 				opts.on '-f', '--files', 'Make the file list text files' do @options << :files end
 				opts.on '-s', '--scan', 'Scan the directories for files and new dirs' do @options << :scan end
-				opts.on '-m', '--move', 'Scan the dirs, move files and make new dirs' do @options << :scan << :move end
+				opts.on '-m', '--move', 'Scan the dirs, move files and make new dirs' do @options << :move end
 				opts.on '-h', '--help', 'Prints this help' do puts @opt_parser; exit end
 			end
 			@opt_parser.banner = 'Usage: filescans [options]'
