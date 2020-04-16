795047edc41640048712daf5d408e527a32c4761
Bob Greenshields <bob@thegreenshields.com>
2020-04-16 17:21:45 +0100

    DirExplorer now ignores .Trash dir by default

--- lib/dir_explorer.rb
+++ lib/dir_explorer.rb
@@ -1,9 +1,14 @@
 module FileScans
+	TRASH_REGEX = /\.Trash/
+	IGNORE_TRASH = ->(dir) { TRASH_REGEX.match(dir.to_s) }
+
 	class DirExplorer
 
-		def initialize(on_file: ->(file, context) {}, on_dir: ->(dir, context) {}, recursive: true, context: nil)
+		def initialize(on_file: ->(file, context) {}, on_dir: ->(dir, context) {},
+									 ignore_dir: IGNORE_TRASH, recursive: true, context: nil)
 			@on_file = on_file
 			@on_dir = on_dir
+			@ignore_dir = ignore_dir
 			@recursive = recursive
 			@context = context
 		end
@@ -15,6 +20,7 @@ module FileScans
 		def call(root)
 			root.each_child do |child|
 				if child.directory?
+					next if @ignore_dir.call(child)
 					@on_dir.call(child, @context)
 					self.clone.call(child) if @recursive
 				elsif child.file?
